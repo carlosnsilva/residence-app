955de5a5acccbd12ddfe1b9615d0ed54e19f016e
carlosnsilva <cmanoel17@gmail.com>
2024-05-26 12:54:20 -0300

    Fix: Subindo tela de home

--- residence-app/residence/lib/homePage.dart
+++ residence-app/residence/lib/homePage.dart
@@ -11,15 +11,35 @@ class homeUser extends StatelessWidget {
       children: <Widget>[
         Padding(
           padding: EdgeInsetsDirectional.all(8),
-          child: Menu(),
+          child: Menu()
         ),
         Padding(
           padding: EdgeInsetsDirectional.all(8),
-          child: Text('Deu certo 1'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.network('https://www.tinyurl.com/5n8ywvw3'), const SizedBox(height: 10), Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Notícias', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                ],
+              )
+            ],
+          )
         ),
         Padding(
           padding: EdgeInsetsDirectional.all(8),
-          child: Text('Deu certo 2'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Image.network('https://www.tinyurl.com/5n8ywvw3'), const SizedBox(height: 10), Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text('Últimas Reservas', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),)
+                ],
+              )
+            ],
+          )
         ),
       ]
     );
